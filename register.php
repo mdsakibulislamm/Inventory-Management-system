3739eff3e659ef906fbc50830256e818f6f4ced5
Md. Sakib Ul Islam <63756507+mdsakibulislamm@users.noreply.github.com>
2023-01-28 16:52:22 +0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!--  css cdn -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<!-- css cdn-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+ 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 	<script type="text/javascript" rel="stylesheet" src="./js/main.js"></script>
+
+	<!--Font awsome-->
+			<script
+				src="https://use.fontawesome.com/releases/v5.13.0/js/all.js"
+			></script>
+		
+
+	<title>Inventory Managment</title>
+ </head>
+<body>
+<div class="overlay"><div class="loader"></div></div>
+	<!-- Navbar -->
+	<?php include_once("./templetes/header.php"); ?>
+	<br/><br/>
+	<div class="container">
+		<div class="card mx-auto" style="width: 30rem;">
+	        <div class="card-header">Register</div>
+		      <div class="card-body">
+		        <form id="register_form" onsubmit="return false" autocomplete="off">
+		          <div class="form-group">
+		            <label for="username">Full Name</label>
+		            <input type="text" name="username" class="form-control" id="username" placeholder="Enter Username">
+		            <small id="u_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="email">Email address</label>
+		            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+		            <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password1">Password</label>
+		            <input type="password" name="password1" class="form-control"  id="password1" placeholder="Password">
+		            <small id="p1_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password2">Re-enter Password</label>
+		            <input type="password" name="password2" class="form-control"  id="password2" placeholder="Password">
+		            <small id="p2_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="usertype">Usertype</label>
+		            <select name="usertype" class="form-control" id="usertype">
+		              <option value="">Choose User Type</option>
+		              <option value="Admin">Admin</option>
+		              <option value="Other">Other</option>
+		            </select>
+		            <small id="t_error" class="form-text text-muted"></small>
+		          </div>
+		          <button type="submit" name="user_register" class="btn btn-primary"><span class="fa fa-user"></span>&nbsp;Register</button>
+		          <span><a href="index.php">Login</a></span>
+		        </form>
+		      </div>
+	      <div class="card-footer text-muted">
+	        
+	      </div>
+	    </div>
+	</div>
+	<br><br>
+<?php include_once("./templetes/footer.php"); ?>
+
+
+
+</body>
+</html>
